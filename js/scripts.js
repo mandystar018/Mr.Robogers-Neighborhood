2071ee750e9e0acf76796c161a2d4fac1658f08f
Amanda Otero <mandystar018@gmail.com>
2020-10-30 11:42:40 -0700

    Having a hard time getting the Beep

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//Business logic
+
 let newArray = [];
 function mrRobo (element) {
-  for( let i = 0; numbers.length; i++) {
-  if (element === numbers[i]) {
+  for( let i = 0; element.length; i++) {
+  if (i === element) {
     newArray.push("Beep!");
-  } else if (element === numbers.indexOf(2)){
+  } else if (i === element){
     newArray.push("Boo!");
   }
   }
 };
 
-console.log(mrRobo(1));
+console.log(mrRobo("0"));
+
+// User Interface logic
