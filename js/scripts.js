1aa2d18da28267a347d7747537f24701eddb299d
Amanda Otero <mandystar018@gmail.com>
2020-10-30 11:55:25 -0700

    Build the UI so i can have a visual idea of the inputted numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,4 @@
 //Business logic
 
-let newArray = [];
-function mrRobo (element) {
-  for( let i = 0; element.length; i++) {
-  if (i === element) {
-    newArray.push("Beep!");
-  } else if (i === element){
-    newArray.push("Boo!");
-  }
-  }
-};
-
-console.log(mrRobo("0"));
 
 // User Interface logic
