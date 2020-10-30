aae53231a1e3fe4ca2caaec8e3ab4991336f876c
Amanda Otero <mandystar018@gmail.com>
2020-10-30 10:26:24 -0700

    trying to figure out how to push beep from my array with forEach but don't know how to call forEach and doing some research

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const newArray = [];
-  const newArray = numbers.map(function(element) {
+const numbers = ["0", "1", "2", 3, 4, 5, 6, 7, 8, 9];
+let newArray = [];
+numbers.forEach(function(element) {
+  if (element === numbers.indexOf(1)) {
+    newArray.push("Beep!");
+  } else if (element === numbers.indexOf(2)){
+    newArray.push("Boo!");
+  }
+});
 
-    return element ;
-  });
-console.log(newArray);
+console.log(function(1));
