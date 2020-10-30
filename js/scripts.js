64c1fbb3c79a4c135de89c543d1ca499a881f689
Amanda Otero <mandystar018@gmail.com>
2020-10-30 08:54:37 -0700

    Add loop using maps to iterate each number in the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-function mrRobo (number){
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-}
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const newArray = [];
+  const newArray = numbers.map(function(element) {
+
+    return element ;
+  });
+console.log(newArray);
